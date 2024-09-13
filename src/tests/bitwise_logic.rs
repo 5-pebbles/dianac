6609ba52634b344fc909f83804ed7c478e0a55c3
5-pebbles <5-pebble@protonmail.com>
2024-09-12 22:57:05 -0400

    feat(tests): add tests for AND;

--- src/tests/bitwise_logic.rs
+++ src/tests/bitwise_logic.rs
@@ -35,6 +35,7 @@ test_builder!(
         assert_eq!(state.c, u6::new(0b000000));
     }
 );
+
 test_builder!(
     not_c,
     "NOT C",
@@ -46,5 +47,94 @@ test_builder!(
         assert_eq!(state.a, u6::new(0b000000));
         assert_eq!(state.b, u6::new(0b000000));
         assert_eq!(state.c, u6::new(0b010010));
+    }
+);
+
+// AND
+
+test_builder!(
+    and_ab,
+    "AND A B\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.b = u6::new(0b110011);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, u6::new(0b100001));
+        assert_eq!(state.b, !u6::new(0b110011));
+        assert_eq!(state.c, u6::new(0b000000));
+    }
+);
+
+test_builder!(
+    and_bc,
+    "AND B C\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.c = u6::new(0b110011);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, u6::new(0b000000));
+        assert_eq!(state.b, u6::new(0b100001));
+        assert_eq!(state.c, !u6::new(0b110011));
+    }
+);
+
+test_builder!(
+    and_ca,
+    "AND C A\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.a = u6::new(0b110011);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, !u6::new(0b110011));
+        assert_eq!(state.b, u6::new(0b000000));
+        assert_eq!(state.c, u6::new(0b100001));
+    }
+);
+
+test_builder!(
+    and_a_immediate,
+    "AND A 0b110011\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, u6::new(0b100001));
+        assert_eq!(state.b, u6::new(0b000000));
+        assert_eq!(state.c, u6::new(0b000000));
+    }
+);
+
+test_builder!(
+    and_b_immediate,
+    "AND B 0b110011\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, u6::new(0b000000));
+        assert_eq!(state.b, u6::new(0b100001));
+        assert_eq!(state.c, u6::new(0b000000));
+    }
+);
+
+test_builder!(
+    and_c_immediate,
+    "AND C 0b110011\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.consume_until_halt()
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.a, u6::new(0b000000));
+        assert_eq!(state.b, u6::new(0b000000));
+        assert_eq!(state.c, u6::new(0b100001));
     }
 );
