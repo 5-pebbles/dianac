a8209dc14b072ada50015dd2f82b5e650cb98c2e
5-pebbles <5-pebble@protonmail.com>
2024-09-15 20:13:44 -0400

    fix(tests): update tests with offset;

--- src/tests/halt_and_nop.rs
+++ src/tests/halt_and_nop.rs
@@ -1,49 +1,49 @@
-use arbitrary_int::u6;
+use crate::{
+    compilation::{compile_to_binary, CompileInfo},
+    emulation::InteractiveState,
+    test_builder,
+};
+use arbitrary_int::{u12, u6};
 
-use crate::{compilation::compile_to_binary, emulation::InteractiveState};
+test_builder!(
+    nop,
+    "NOP\nHLT",
+    |state| {
+        state.consume_until_halt();
+    },
+    |state, machine_code_result| {
+        assert_eq!(machine_code_result.diagnostics.len(), 0);
+        assert_eq!(
+            (state.a, state.b, state.c),
+            (u6::new(0), u6::new(0), u6::new(0))
+        );
+    }
+);
 
-#[test]
-fn nop() {
-    let mut state = InteractiveState::new();
+test_builder!(
+    halt,
+    "HLT",
+    |state| {
+        state.consume_until_halt();
+    },
+    |state, machine_code_result| {
+        assert_eq!(machine_code_result.diagnostics.len(), 0);
+        assert_eq!(state.program_counter.as_tuple(), (u6::new(0), u6::new(0)));
+        assert_eq!(
+            (state.a, state.b, state.c),
+            (u6::new(0), u6::new(0), u6::new(0))
+        );
+    }
+);
 
-    let mc_result = compile_to_binary("NOP");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-
-    state.memory.store_array(0, &mc_result.binary);
-    state.consume_instruction();
-
-    assert_eq!(
-        (state.a, state.b, state.c),
-        (u6::new(0), u6::new(0), u6::new(0))
-    );
-}
-
-#[test]
-fn halt() {
-    let mut state = InteractiveState::new();
-
-    let mc_result = compile_to_binary("HLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-
-    state.memory.store_array(0, &mc_result.binary);
-    state.consume_until_halt();
-
-    assert_eq!(state.program_counter.as_tuple(), (u6::new(0), u6::new(0)));
-    assert_eq!(
-        (state.a, state.b, state.c),
-        (u6::new(0), u6::new(0), u6::new(0))
-    );
-}
-
-#[test]
-fn halt_after_nop() {
-    let mut state = InteractiveState::new();
-
-    let mc_result = compile_to_binary("NOP\nHLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-
-    state.memory.store_array(0, &mc_result.binary);
-    state.consume_until_halt();
-
-    assert_eq!(state.program_counter.as_tuple(), (u6::new(0), u6::new(1)));
-}
+test_builder!(
+    halt_after_nop,
+    "NOP\nHLT",
+    |state| {
+        state.consume_until_halt();
+    },
+    |state, machine_code_result| {
+        assert_eq!(machine_code_result.diagnostics.len(), 0);
+        assert_eq!(state.program_counter.as_tuple(), (u6::new(0), u6::new(1)));
+    }
+);
--- src/tests/shift_and_rotate.rs
+++ src/tests/shift_and_rotate.rs
@@ -1,83 +1,154 @@
-use arbitrary_int::u6;
+use crate::{
+    compilation::{compile_to_binary, CompileInfo},
+    emulation::InteractiveState,
+    test_builder,
+};
+use arbitrary_int::{u12, u6};
 
-use crate::{compilation::compile_to_binary, emulation::InteractiveState};
+// ROL tests
+test_builder!(
+    rol_a,
+    "ROL A\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011011));
+    }
+);
 
-#[test]
-fn rol() {
-    let mut state = InteractiveState::new();
-    let mc_result = compile_to_binary("ROL A\nHLT\nROL B\nHLT\nROL C\nHLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-    state.memory.store_array(0, &mc_result.binary);
-    state.a = u6::new(0b101101);
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011011));
-    state.c = u6::new(0);
-    state.b = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011011));
-    state.c = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011011));
-}
+test_builder!(
+    rol_b,
+    "ROL B\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011011));
+    }
+);
 
-#[test]
-fn ror() {
-    let mut state = InteractiveState::new();
-    let mc_result = compile_to_binary("ROR A\nHLT\nROR B\nHLT\nROR C\nHLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-    state.memory.store_array(0, &mc_result.binary);
-    state.a = u6::new(0b101101);
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b110110));
-    state.c = u6::new(0);
-    state.b = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b110110));
-    state.c = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b110110));
-}
+test_builder!(
+    rol_c,
+    "ROL C\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011011));
+    }
+);
 
-#[test]
-fn shl() {
-    let mut state = InteractiveState::new();
-    let mc_result = compile_to_binary("SHL A\nHLT\nSHL B\nHLT\nSHL C\nHLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-    state.memory.store_array(0, &mc_result.binary);
-    state.a = u6::new(0b101101);
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011010));
-    state.c = u6::new(0);
-    state.b = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011010));
-    state.c = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b011010));
-}
+// ROR tests
+test_builder!(
+    ror_a,
+    "ROR A\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b110110));
+    }
+);
 
-#[test]
-fn shr() {
-    let mut state = InteractiveState::new();
-    let mc_result = compile_to_binary("SHR A\nHLT\nSHR B\nHLT\nSHR C\nHLT");
-    assert_eq!(mc_result.diagnostics.len(), 0);
-    state.memory.store_array(0, &mc_result.binary);
-    state.a = u6::new(0b101101);
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b010110));
-    state.c = u6::new(0);
-    state.b = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b010110));
-    state.c = u6::new(0b101101);
-    state.consume_instruction();
-    state.consume_until_halt();
-    assert_eq!(state.c, u6::new(0b010110));
-}
+test_builder!(
+    ror_b,
+    "ROR B\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b110110));
+    }
+);
+
+test_builder!(
+    ror_c,
+    "ROR C\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b110110));
+    }
+);
+
+// SHL tests
+test_builder!(
+    shl_a,
+    "SHL A\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011010));
+    }
+);
+
+test_builder!(
+    shl_b,
+    "SHL B\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011010));
+    }
+);
+
+test_builder!(
+    shl_c,
+    "SHL C\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b011010));
+    }
+);
+
+// SHR tests
+test_builder!(
+    shr_a,
+    "SHR A\nHLT",
+    |state| {
+        state.a = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b010110));
+    }
+);
+
+test_builder!(
+    shr_b,
+    "SHR B\nHLT",
+    |state| {
+        state.b = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b010110));
+    }
+);
+
+test_builder!(
+    shr_c,
+    "SHR C\nHLT",
+    |state| {
+        state.c = u6::new(0b101101);
+        state.consume_until_halt();
+    },
+    |state, _machine_code_result| {
+        assert_eq!(state.c, u6::new(0b010110));
+    }
+);
